e6a9f21129df2d048ac2a26f7ff368da0f3d8abf
William Barela <william.barela.dev@gmail.com>
2020-11-13 10:01:02 -0600

    ignore built gem files

--- lib/arpeggio/version.rb
+++ lib/arpeggio/version.rb
@@ -1,4 +1,4 @@
 module Arpeggio
-  # ||= could be used to avoid warning error in pry; however, this should not be done to avoid breaking the gem assuming the user updates the version
-  VERSION = "0.1.0"
+  # ||= added
+  VERSION ||= "0.1.2"
 end
