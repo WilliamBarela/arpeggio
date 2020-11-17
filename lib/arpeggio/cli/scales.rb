7cdef8d91d1aed8178f87621203f94c2f4de99cb
William Barela <william.barela.dev@gmail.com>
2020-11-16 23:19:04 -0600

    add scale and tone constants

---
+++ lib/arpeggio/cli/scales.rb
@@ -0,0 +1,4 @@
+class Scales
+  INTERVAL = ["r", "m2", "M2", "m3", "M3", "P4", "TT", "P5", "m6", "M6", "m7", "M7"]
+  TWELVE_TONE_SCALE = ["C", "C♯/D♭", "D", "D♯/E♭", "E", "F",  "F♯/G♭", "G", "G♯/A♭", "A", "A♯/B♭", "B"]
+end
